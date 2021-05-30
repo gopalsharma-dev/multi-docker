dc24f85bf75205b4d3304358cee9f660c2fbf91a
gopalsharma62 <gopusharma62@gmail.com>
2021-05-30 21:19:52 +0530

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,9 @@ import Fib from './Fib';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style="font-color:white;">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
+            Fibnonacci Series By Index
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
